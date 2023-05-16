5b491bf625338ffb86377c5b0df6a44356e8ac60
mohamed-jebali <mohamedjebali992010@gmail.com>
2023-05-16 15:50:55 +0200

    working on console.log print fizz buzz

--- js/script.js
+++ js/script.js
@@ -1,10 +1,15 @@
 
 for(let number = 1; number <= 100; number++){
     console.log(number);
-    if(number % 3 === 0){
-        console.log(number + "Fizz")
+
+    if(number % 5 === 0 && number % 3 === 0){
+        console.log("FizzBuzz")
     }
-    else if (number % 5 === 0)
-    console.log(number + "Buzz")
-    // document.getElementById("output").innerHTML += index;
+    else if(number % 3 === 0){
+        console.log("Fizz")
+    }
+    else if (number % 5 === 0){
+    console.log("Buzz")
+    }
+    // document.getElementById("output").innerHTML += ;
 }
