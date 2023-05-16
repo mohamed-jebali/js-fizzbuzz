3e6fa999f9bfc40caf524797882c2a073f9cdcb3
mohamed-jebali <mohamedjebali992010@gmail.com>
2023-05-16 15:20:41 +0200

    added fizz buzz conditions

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 
 for(let number = 1; number <= 100; number++){
     console.log(number);
+    if(number % 3 === 0){
+        console.log(number + "Fizz")
+    }
+    else if (number % 5 === 0)
+    console.log(number + "Buzz")
     // document.getElementById("output").innerHTML += index;
 }
